1276c37f0198e15976f93edf00e91e08ce59859a
DrJuanch <juanjo88023@gmail.com>
2023-02-13 16:02:42 -0500

    Schemas finished

--- model/model.js
+++ model/model.js
@@ -1,56 +1,120 @@
 const mongoose = require("mongoose");
 
+// Schemas
 
 mongoose.set('useCreateIndex', true);
-const actorSchema = new mongoose.Schema({
-    act_name:{
-        type:String,
-        require:true,
-        unique:true
-    },
-    act_lastName:{
-        type:String,
-        require:true,
-        unique:true
-    },
-    act_gender:{
-        type:String,
-        require:true,
-        unique:true
-    },
-},
+
+const movieSchema = new mongoose.Schema(
     {
-    timestamps:true //This create a creation date and update date
+        mov_title:{
+            type: 'string',
+            required:true,
+            unique:true
+        },
+        mov_year:{
+            type: integer,
+            required: true,
+        },
+        mov_time:{
+            type: integer
+        },
+        mov_lang:{
+            type: String
+        },
+        mov_dt_rel:{
+            type: Date
+        },
+        mov_rel_country:{
+            type:String
+        }
+    },
+    {
+        timestamps:true
     }
 )
 
-const directorSchema = new mongoose.Schema({
-    dir_name:{
-        type:String,
-        require:true
+const movieDirectionSchema = new mongoose.Schema(
+    {
+        dir_info:{
+            type:{
+                dir_name:{
+                    type:String,
+                    required:true
+                },
+                dir_lastname:{
+                    type:String
+                }
+            },
+            required: true
+        },
+        mov_id:{
+            type: mongoose.Types.ObjectId,
+            required:true
+        }
     },
-    dir_lastname:{
-        type:String,
-        require:true
-    }
-},
     {
         timestamps:true
-})
-
-const reviewerSchema = new mongoose.Schema({
-    rev_name:{
-        type:String,
-        require:true
     }
-},
+)
+
+const movieGenderSchema = new mongoose.Schema(
+    {
+        mov_id:{
+            type: mongoose.Types.ObjectId,
+            required:true
+        },
+        gender:{
+            type:{
+                gen_title:{
+                    type:String,
+                    required:true
+                }
+            }
+        }
+    },
     {
         timestamps:true
-})
-
-const genderSchema = new mongoose.Schema({
-
-    gen_tittle:{
-        type:String
     }
-})
+)
+
+const raitingSchema = new mongoose.Schema(
+    {
+        mov_id:{
+            type: mongoose.Types.ObjectId
+        },
+        reviewer:{
+            type:{
+                rev_name:{
+                    type:String,
+                    require:true
+                }
+            }
+        },
+        rev_stars: {
+            type:integer
+        },
+        num_o_raitings: {
+            type:integer
+        }
+    }
+)
+
+const movieCastSchema= new mongoose.Schema(
+    {
+        actor:{
+            type:{
+                act_name:{
+                    type:String,
+                    required:true,
+                },
+                act_lastName:{
+                    type:String,
+                }
+            }
+        },
+        mov_id:{
+            type: mongoose.Types.ObjectId
+        }
+    }
+)
+
