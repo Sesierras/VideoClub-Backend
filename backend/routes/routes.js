c2c205d300b65097357ed18085796a1c8cae008b
DrJuanch <juanjo88023@gmail.com>
2023-02-14 15:43:09 -0500

    Database

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,21 +2,21 @@ const router = require('express').Router();
 const model = require('../model/model');
 
 router.get("/", (req, res) => {
-    toDo.find((err, result) => {
+    model.find((err, result) => {
         if (err) throw new Error(err);
         res.json(result);
     });
 });
 
 router.post("/", (req, res) => {
-    toDo.create(req.body, (err, result) => {
+    model.create(req.body, (err, result) => {
         if (err) throw new Error(err);
         res.send(result);
     });
 });
 
 router.delete("/:id", (req,res) => {
-    toDo.findOneAndRemove({_id:req.body.id}, (err,result) =>{
+    model.findOneAndRemove({_id:req.body.id}, (err,result) =>{
         if (err) throw new Error(err);
         res.end();
     })
