ef0606b9a98af3d1b59781939801fd825230df92
Mafesita23 <116750999+Mafesita23@users.noreply.github.com>
2023-02-15 14:38:06 -0500

    Add table, tailwind and mode dark

--- Frontend/videoCLub/src/App.jsx
+++ Frontend/videoCLub/src/App.jsx
@@ -1,32 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Table from './components/Table';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState(null);
+
+	useEffect(() => {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
+
+	const handleThemeSwitch = (e) => {
+		setTheme(theme === 'dark' ? 'light' : 'dark');
+	};
+
+	useEffect((e) => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+ 
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Table/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+     <>
+			<button
+				type="button"
+				onClick={handleThemeSwitch}
+				className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md"
+			>
+				{theme === 'dark' ? '🌚' : '🌝'}
+			</button>
+			<div className="bg-white dark:bg-slate-700 min-h-screen font-inter ">
+				<div className="max-w-6xl w-11/12 mx-auto">
+					
+				</div>
+			</div>
+		</>
     </div>
   )
 }
---
+++ Frontend/videoCLub/src/components/Table.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+
+
+
+function Table() {
+	return (
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+            <th scope="col" class="px-6 py-3">
+                Acciones
+            </th>
+            <th scope="col" class="px-6 py-3">
+                ID
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Titulo
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Año
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Tiempo
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Lenguaje
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Fecha de lanzamiento
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Pais
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Imagen
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Apple MacBook Pro 17"
+            </th>
+            <td class="px-6 py-4">
+                Sliver
+            </td>
+            <td class="px-6 py-4">
+                Laptop
+            </td>
+            <td class="px-6 py-4">
+                $2999
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+        <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Microsoft Surface Pro
+            </th>
+            <td class="px-6 py-4">
+                White
+            </td>
+            <td class="px-6 py-4">
+                Laptop PC
+            </td>
+            <td class="px-6 py-4">
+                $1999
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Magic Mouse 2
+            </th>
+            <td class="px-6 py-4">
+                Black
+            </td>
+            <td class="px-6 py-4">
+                Accessories
+            </td>
+            <td class="px-6 py-4">
+                $99
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+        <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Google Pixel Phone
+            </th>
+            <td class="px-6 py-4">
+                Gray
+            </td>
+            <td class="px-6 py-4">
+                Phone
+            </td>
+            <td class="px-6 py-4">
+                $799
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Apple Watch 5
+            </th>
+            <td class="px-6 py-4">
+                Red
+            </td>
+            <td class="px-6 py-4">
+                Wearables
+            </td>
+            <td class="px-6 py-4">
+                $999
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</div>
+);
+}
+
+export default Table;
